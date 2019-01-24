30927709689040ed77b112950d09a5ee1a04c16c
Suha Kazi <sukazi.ucsd.edu>
2019-01-24 10:55:59 -0800

    lab 3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,27 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").toggleClass("active");
 	});
 
+	$("a.thumbnail").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+$('#project1').click(projectClick); 
+
+function projectClick(e) {
+	e.preventDefault();
+
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+		if (description.length == 0) {
+			$('#project1').text("Here we go again!!")
+			$(containingProject).append("<div class = project-description'><p>Description of the project.</p></div>");
+	} 
+	else {
+		$(description).fadeOut()
+	}
 }
